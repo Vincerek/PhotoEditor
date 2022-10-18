30d664fbd21dbd11bd9b47709323b58dcc06d5d6
jp2137 <sbaiyassine2004@gmail.com>
2022-10-18 14:54:48 +0200

    Beta 1.1 Obracanie tła pionowo i poziomo Bugs: Crash w momencie kiedy wartość przekroczy 255 lub jest ujemna.

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
-
     fun ChangeTint(view: View) {
         val imageView = findViewById<ImageView>(R.id.imageView)
         val cAlpha = findViewById<EditText>(R.id.AlphaValue)
@@ -22,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         val cGreen = findViewById<EditText>(R.id.GreenValue)
         val cBlue = findViewById<EditText>(R.id.BlueValue)
 
-        val AlphaV = cAlpha.text.toString()
+        val alphaV = cAlpha.text.toString()
         val redV = cRed.text.toString()
         val greenV = cGreen.text.toString()
         val blueV = cBlue.text.toString()
@@ -30,11 +29,36 @@ class MainActivity : AppCompatActivity() {
         var red = 0
         var green = 0
         var blue = 0
-        Integer.parseInt(AlphaV)
+
+        if (alphaV == ""){
+            alpha = 0
+        } else if(Integer.parseInt(alphaV)<0){
+            alpha = 0
+        } else if (Integer.parseInt(alphaV)>255){
+            alpha = 255
+        }else  alpha = Integer.parseInt(alphaV)
+
+        if (redV == ""){
+            red = 0
+        } else if(Integer.parseInt(redV)<0){
+            red = 0
+        } else if (Integer.parseInt(redV)>255){
+            red = 255
+        }else  red = Integer.parseInt(redV)
+        //Integer.parseInt(alphaV)
         //Integer.parseInt(redV)
         //Integer.parseInt(greenV)
         //Integer.parseInt(blueV)
 
-        imageView.setColorFilter(Color.argb(Integer.parseInt(AlphaV), Integer.parseInt(redV), Integer.parseInt(greenV), Integer.parseInt(blueV)));
+        imageView.setColorFilter(Color.argb(alpha, red, Integer.parseInt(greenV), Integer.parseInt(blueV)));
+    }
+    fun flipX(view: View) {
+        val flip = findViewById<ImageView>(R.id.imageView)
+        flip.scaleX = flip.scaleX * -1
+    }
+
+    fun flipY(view: View) {
+        val flip = findViewById<ImageView>(R.id.imageView)
+        flip.scaleY = flip.scaleY * -1
     }
 }
