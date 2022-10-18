5a0211c2a260feaaf4177398b9f251a734f440cf
jp2137 <sbaiyassine2004@gmail.com>
2022-10-18 13:58:22 +0200

    Beta 1.0 Zmiana tła działa za pomocą buttona Bugs: Crash w momencie kiedy wartość przekroczy 255 lub jest ujemna.

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.photoeditor
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun ChangeTint(view: View) {
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val cAlpha = findViewById<EditText>(R.id.AlphaValue)
+        val cRed = findViewById<EditText>(R.id.RedValue)
+        val cGreen = findViewById<EditText>(R.id.GreenValue)
+        val cBlue = findViewById<EditText>(R.id.BlueValue)
+
+        val AlphaV = cAlpha.text.toString()
+        val redV = cRed.text.toString()
+        val greenV = cGreen.text.toString()
+        val blueV = cBlue.text.toString()
+        var alpha = 0
+        var red = 0
+        var green = 0
+        var blue = 0
+        Integer.parseInt(AlphaV)
+        //Integer.parseInt(redV)
+        //Integer.parseInt(greenV)
+        //Integer.parseInt(blueV)
+
+        imageView.setColorFilter(Color.argb(Integer.parseInt(AlphaV), Integer.parseInt(redV), Integer.parseInt(greenV), Integer.parseInt(blueV)));
     }
 }
